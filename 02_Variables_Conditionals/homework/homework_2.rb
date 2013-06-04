9cc7e7fb4d4639a46237891c5204a6474cdfd334
Celsian <celsian@gmail.com>
2013-06-04 12:56:07 -0700

    Homework #2 Upload

---
+++ 02_Variables_Conditionals/homework/homework_2.rb
@@ -0,0 +1,42 @@
+#My first homework assignment is located at: http://celsian.tumblr.com/
+
+#--- HOMEWORK START ----
+def guessMethod		#This method gets the user input and verifies it falls within acceptable limits.
+	print "Enter your guess: "
+	guess = gets.chomp.to_i #Takes the guess, chomps it, and type casts it to a Fixnum
+	
+	if guess >= 1 && guess <= 10 #Verifies guess is within acceptable limits.
+		guess #Returns it.
+	else
+		print "Invalid guess, please pick a number between 1 & 10. "
+		guessMethod	#Method calls itself again if the input is not acceptable.
+	end
+end
+
+puts "Welcome to the Secret Number Game, created by Lucas."	#Welcomes the user
+print "Enter your name: "	#Requests the users name
+name = gets.chomp 	#Gets the name and chomps it
+puts "Hi #{name}!"	#Tells the user Hello and prints the users name.
+puts "You have three chances to guess a number between 1 and 10."	#Instructions for the game.
+
+secret_number = rand(10)+1 #Calculates the secret number
+i = 3 #Sets the guesses to three
+guess = 0 #0 initializes the users guess
+
+while(i>0 && guess != secret_number) #Loops until the user is out of guesses & user has not guessed the secret_number
+	i -= 1 #Increments the number of guesses
+	guess = guessMethod #Calls the guess method and sets "guess" to the users guessed value
+	if guess > secret_number #If the guess is too high...
+		puts "Too high! You have #{i} guesses left."
+	elsif guess < secret_number #If the guess is too low...
+		puts "Too low! You have #{i} guesses left."
+	elsif guess == secret_number #If they guessed correctly...
+		puts "Congratulations #{name}, you guessed the Secret Number! You win!"
+	end
+end
+
+if guess != secret_number #If case for failure to guess secret_number
+	puts "Sorry #{name}, you've run out of guesses, you lose. The number was #{secret_number}."
+end
+#--- HOMEWORK END ----
+
