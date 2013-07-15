b577f1e93e5bd6cc5cd78e9ae654f99953a0f004
Celsian <celsian@gmail.com>
2013-07-15 07:41:36 -0700

    Washoes commit

--- My_Rails_Apps/washoes/app/models/game.rb
+++ My_Rails_Apps/washoes/app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
   has_and_belongs_to_many :players
-  has_and_belongs_to_many :winners, class_name: "Player"
+  has_one :winner, class_name: "Player"
   belongs_to :bracket
   
 end
